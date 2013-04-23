af98fd73016e0b4cf02b1b2a5e3321f01228643d
Michael Williamson <mike@zwobble.org>
2013-04-23 21:35:46 +0100

    Add upload box to post editor

--- mammoth/mammoth.php
+++ mammoth/mammoth.php
@@ -6,3 +6,23 @@ Author: Michael Williamson
 Author URI: http://mike.zwobble.org/
 License: BSD 2-clause
 */
+
+add_action( 'add_meta_boxes', 'mammoth_add_post_meta_box' );
+
+function mammoth_add_post_meta_box() {
+    $screens = array( 'post', 'page' );
+    foreach ($screens as $screen) {
+        add_meta_box(
+            'mammoth_add_post',
+            __( 'Mammoth' ),
+            'mammoth_render_editor_box',
+            $screen
+        );
+    }
+}
+
+function mammoth_render_editor_box( $post ) {
+    echo '<label>Select docx file:';
+    echo '<input type="file" id="mammoth-docx-upload" />';
+    echo '</label>';
+}
