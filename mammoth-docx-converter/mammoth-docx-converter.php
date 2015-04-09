6f761a6b3dfc7e45ddd263915a117e2a23d463b9
Michael Williamson <mike@zwobble.org>
2015-04-09 08:28:11 +0100

    Bump version to 0.1.15

--- mammoth-docx-converter/mammoth-docx-converter.php
+++ mammoth-docx-converter/mammoth-docx-converter.php
@@ -3,7 +3,7 @@
 Plugin Name: Mammoth .docx converter
 Plugin URI: https://github.com/mwilliamson/mammoth-wordpress-plugin
 Description: Mammoth converts semantically marked up .docx documents to simple and clean HTML.
-Version: 0.1.14
+Version: 0.1.15
 Author: Michael Williamson
 Author URI: http://mike.zwobble.org/
 License: BSD 2-clause
