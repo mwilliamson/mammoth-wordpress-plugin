391b4b9b6359ed9e3eb065caa37260c0d1e08e53
Michael Williamson <mike@zwobble.org>
2018-12-18 19:18:57 +0000

    Pin to MySQL 5.7

--- tests/start_mysql.py
+++ tests/start_mysql.py
@@ -6,7 +6,7 @@ from .util import docker_run, docker_container_mysql_name, mysql_password
 def main():
     with docker_run(
         name=docker_container_mysql_name,
-        image="mysql",
+        image="mysql:5.7",
         env={"MYSQL_ROOT_PASSWORD": mysql_password},
         volumes={os.path.join(os.path.dirname(__file__), "_mysql-data"): "/var/lib/mysql"},
     ):
